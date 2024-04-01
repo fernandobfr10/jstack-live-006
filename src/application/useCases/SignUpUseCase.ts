6318f878104c60101d5760579bc72eeb448a0e6b
Fernando Junior <fernandobfr10@gmail.com>
2024-04-01 20:05:42 -0300

    feat: :sparkles: create signup use case

---
+++ src/application/useCases/SignUpUseCase.ts
@@ -0,0 +1,34 @@
+import { hash } from "bcryptjs";
+
+import { AccountAlreadyExists } from "../errors/AccountAlreadyExists";
+import { prismaClient } from "../libs/prismaClient";
+
+interface IInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
+type IOutput = void;
+
+export class SignUpUseCase {
+  async execute({ name, email, password }: IInput): Promise<IOutput> {
+    const accountAlreadyExists = await prismaClient.account.findUnique({
+      where: { email },
+    });
+
+    if (accountAlreadyExists) {
+      throw new AccountAlreadyExists();
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    await prismaClient.account.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
